2fc5930855cf852db2f029c598962ee51f14ff80
Prajith Maniyan <prajith.manian@gmail.com>
2017-05-06 02:24:06 +1000

    added legend keys for sentiments on maps

--- public/scripts/heatMap.js
+++ public/scripts/heatMap.js
@@ -5,30 +5,41 @@ let data = $('.data').text();
 data = JSON.parse(data);
 console.log("data. in google Maps: ", data);
 var coords = [];
+var senti = [];
 
 for (var i = 0; i < data.length; i++) {
     if (data[i].value.coords) {
         //coords.push()
         console.log("in heat map coords: ", data[i].value.coords);
-        coords.push({lat: data[i].value.coords[0], lng: data[i].value.coords[1]})
+        coords.push({lat: data[i].value.coords.coordinates[0], lng: data[i].value.coords.coordinates[1]})
+        senti.push(data[i].value.sentiment)
     } else if (data[i].value.place) {
         //coords.push()
         console.log("in heat map places bouding box coords: ", data[i].value.place.bounding_box.coordinates[0][0]);
         coords.push({lat: data[i].value.place.bounding_box.coordinates[0][0][1], lng: data[i].value.place.bounding_box.coordinates[0][0][0]})
+        senti.push(data[i].value.sentiment)
     }
 }
 
 
-function addMarker(location, map) {
+function addMarker(location, map, senti) {
         // Add the marker at the clicked location, and add the next-available label
         // from the array of alphabetical characters.
 
         //http://maps.google.com/mapfiles/ms/icons/blue-dot.png
+        var icon;
+        if (senti == "positive") {
+          icon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+        } else if (senti == "negative") {
+          icon = ''
+        } else if (senti == "neutral"){
+          icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+        }
         var marker = new google.maps.Marker({
           position: location,
           //label: labels[labelIndex++ % labels.length],
           title:"Hello",
-          icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+          icon: icon,
           map: map
         });
 }
@@ -43,7 +54,7 @@ function initMap() {
   });
 
   for (var i = 0; i < coords.length; i++) {
-     addMarker(coords[i], map);
+     addMarker(coords[i], map, senti[i]);
   }
 
 
