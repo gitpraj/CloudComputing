27fb59653d66be12fe423d942cde92f967187c45
Prajith Maniyan <prajith.manian@gmail.com>
2017-05-06 03:30:26 +1000

    bounding box is not ready yet

--- public/scripts/piechart.js
+++ public/scripts/piechart.js
@@ -1,6 +1,6 @@
 
 let data2 = $('.data').text();
-console.log("data2", data2);
+// console.log("data2", data2);
 data2 = JSON.parse(data2);
 //console.log("data2: ", data2);
 
@@ -43,5 +43,42 @@ var data = {
   var myPieChart = new Chart(ctx,{
       type: 'pie',
       data: data,
+      // options: options
+  });
+
+
+
+
+  var ctx2 = document.getElementById("mychart1");
+  var data22 = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+          {
+              label: "My First dataset",
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1,
+              data: [65, 59, 80, 81, 56, 55, 40],
+          }
+      ]
+  };
+
+  var myBarChart = new Chart(ctx2, {
+      type: 'bar',
+      data: data22,
       // options: options
   });
