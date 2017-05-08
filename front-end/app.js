6b6518c688ff4d0525a0ae26cc3e52daf5977d04
Prajith Maniyan <prajith.manian@gmail.com>
2017-05-08 23:23:53 +1000

    tried to fix couch db view time

--- front-end/app.js
+++ front-end/app.js
@@ -6,6 +6,8 @@ var bodyParser = require("body-parser");
 var jsonfile = require('jsonfile')
 
 var db = require('./couch_db.js')
+//require('./mongo_db.js')
+// var Afl = require('./mongo_models.js');
 
 app.use(bodyParser.urlencoded({limit: '1000mb',extended: true, parameterLimit: 500000}));
 app.use(bodyParser.json({limit: '1000mb'}))
@@ -52,29 +54,31 @@ app.get("/landing", function(req, res) {
 
 
 app.post("/landing/afl/:teamName", function(req, res) {
-  // console.log("teamName: ", req.params.teamName);
-  // let body = req.body;
-  // let data = JSON.parse(body.data);
-  // console.log("before rendering the teams url: ", data[1]);
-  // //res.render("aflTeam.ejs", {teamName:req.params.teamName, data:(data)});
+
+   console.log("teamName: ", req.params.teamName);
+  let body = req.body;
+  let data = JSON.parse(body.data);
+  console.log("in landing/afl/teamName: ", data)
+  //console.log("before rendering the teams url: ", data);
+  res.render("aflTeam.ejs", {teamName:req.params.teamName, data:(data)});
   let teamName = req.params.teamName
-    db.db.view('aflteams/team', function (err, response) {
-            if (err) {
-              console.log("error")
-            } else {
-              console.log(response[0].key)
-              console.log("value: ", response[0])
-              var teamObj = []
-              for (var i = 0; i < response.length; i++) {
-                  if (response[i].key == teamName) {
-                    teamObj.push(response[i])
-                  } else {
-                    //do Nothing
-                  }
-              }
-              res.render("aflTeam.ejs", {teamName:teamName, data:(JSON.stringify(teamObj))});
-            }
-    });
+    // db.db.view('aflteams/team', function (err, response) {
+    //         if (err) {
+    //           console.log("error")
+    //         } else {
+    //           console.log(response[0].key)
+    //           console.log("value: ", response[0])
+    //           var teamObj = []
+    //           for (var i = 0; i < response.length; i++) {
+    //               if (response[i].key == teamName) {
+    //                 teamObj.push(response[i])
+    //               } else {
+    //                 //do Nothing
+    //               }
+    //           }
+    //           res.render("aflTeam.ejs", {teamName:teamName, data:(JSON.stringify(teamObj))});
+    //         }
+    // });
 });
 app.get("/landing/afl/:teamName", function(req, res) {
     db.db.view('aflteams/team', function (err, response) {
@@ -91,7 +95,7 @@ app.get("/landing/afl/:teamName", function(req, res) {
 
 
 app.post("/landing/afl", function(req, res) {
-  db.db.view('aflteams/team', function (err, response) {
+  db.db.view('aflteams/team', {group: false, reduce: false}, function (err, response) {
           if (err) {
             console.log("error")
           } else {
@@ -101,8 +105,10 @@ app.post("/landing/afl", function(req, res) {
                   // do nothing
                 } else {
                   sendKeys.push(response[i].key);
+
                 }
             }
+
             console.log("sendKeys: ", sendKeys)
             res.render("afl.ejs", {data:JSON.stringify(response), dataRaw: response, names:sendKeys});
           }
--- front-end/couch_db.js
+++ front-end/couch_db.js
@@ -3,19 +3,19 @@ var tunnel = require('tunnel-ssh');
 var cradle = require('cradle');
 
 
-var config = {
-    username:"ubuntu",
-    host:"115.146.93.148",
-    agent : process.env.SSH_AUTH_SOCK,
-    privateKey:require('fs').readFileSync('C:/Users/prajith/ccc2017-team8.pem'),
-
-    port:22,
-    dstPort:27017,
-};
+// var config = {
+//     username:"ubuntu",
+//     host:"115.146.93.148",
+//     agent : process.env.SSH_AUTH_SOCK,
+//     privateKey:require('fs').readFileSync('C:/Users/prajith/ccc2017-team8.pem'),
+//
+//     port:22,
+//     dstPort:27017,
+// };
 
 
 var connection = new(cradle.Connection)('http://115.146.93.148', {auth: { username: 'admin', password: 'password' }});
-var db = connection.database('tweets');
+var db = connection.database('tweets_temp');
 db.exists(function (err, exists) {
     if (err) {
       console.log('error', err);
